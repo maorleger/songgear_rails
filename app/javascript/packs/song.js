da669ee009484453dc8f1e0f8b61af4ff1dd8285
Maor Leger <maor.leger@gmail.com>
2017-10-14 13:01:22 -0600

    Rename elm pack

--- app/javascript/packs/song.js
+++ app/javascript/packs/song.js
@@ -1,7 +1,3 @@
-// Run this example by adding <%= javascript_pack_tag "hello_elm" %> to the
-// head of your layout file, like app/views/layouts/application.html.erb.
-// It will render "Hello Elm!" within the page.
-
 import Elm from '../Main';
 
 document.addEventListener('turbolinks:load', () => {

32f6facbe7a13b8f757e3904528ee05355d47260
Maor Leger <maor.leger@gmail.com>
2017-10-14 11:00:46 -0600

    Seeds for song

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+[Song].each do |c|
+  c.destroy_all
+end
+
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+Song.create!(
+  title: "Hey Joe",
+  note: "Play in standard tuning\n\nAnd some other note here",
+  youtube_url: "https://www.youtube.com/watch?v=9hD44jOQG4Q",
+)

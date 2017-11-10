5e10be1cf4acc535988176dca4f377d5ebf2366e
Maor Leger <maor.leger@gmail.com>
2017-11-10 16:06:17 -0700

    repository specs for update

--- spec/repository/repository_spec.rb
+++ spec/repository/repository_spec.rb
@@ -54,4 +54,25 @@ RSpec.describe Repository do
       end
     end
   end
+
+  describe "#update" do
+    let(:song) { create(:song) }
+    describe "when the object is valid" do
+      it "updates the object and returns it" do
+        actual = subject.update(song, title: "foo", note: "hello")
+
+        expect(actual.title).to eq("foo")
+        expect(actual.note).to eq("hello")
+        expect(actual.valid?).to eq(true)
+      end
+    end
+
+    describe "when the object is invalid" do
+      it "returns nil and does not save the object" do
+        actual = subject.update(song, title: nil)
+
+        expect(actual.valid?).to eq(false)
+      end
+    end
+  end
 end
